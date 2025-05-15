ebca76cb618a8a4566b63c87c4825c07014e5af5
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-05-15 10:46:26 +0530

    return 2 elements that only appear once

---
+++ arrays/single-number-3.java
@@ -0,0 +1,22 @@
+package arrays;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int[] res=new int[2];
+        int xor=0;
+        for(int num:nums){
+            xor=xor^num;
+        }
+        int diff=xor & -xor;
+
+        for(int num:nums){
+            if((num & diff) !=0){
+                res[0]=res[0]^num;
+            }
+            else{
+                res[1]=res[1]^num;
+            }
+        }
+        return res;
+    }
+}
